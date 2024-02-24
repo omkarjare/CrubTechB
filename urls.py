598a32942b25e97439b9c56b6b00f8c86949076a
omkarjare <123889556+omkarjare@users.noreply.github.com>
2024-02-24 12:41:22 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
-from .views import *
+"""
+URL configuration for project1 project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('ov/', order_view, name='order_url'),
-    path('sv/', show_view, name='show_url'),
-    path('uv/<int:pk>/', update_view, name='update_url'),
-    path('dv/<int:pk>/', delete_view, name='delete_url')
+    path('admin/', admin.site.urls),
+    path('', include('app1.urls'))
 ]
